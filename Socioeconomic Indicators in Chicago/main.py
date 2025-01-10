8edcfce2ddcaa53c0a8850791e858e090bf18b4e
Md Hasanuzzaman <dr.has82@gmail.com>
2025-01-10 22:42:41 +0600

    Requirments file updated

---
+++ Socioeconomic Indicators in Chicago/main.py
@@ -0,0 +1,7 @@
+import seaborn as sns
+import pandas as pd
+import prettytable
+
+print("Seaborn version:", sns.__version__)
+print("Pandas version:", pd.__version__)
+print("PrettyTable version:", prettytable.__version__)
